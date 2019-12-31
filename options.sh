2acd22a27084142540a35f12f084deaa7f61472c
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2019-12-31 19:06:00 +0300

    set openssl version to 1.0.1g

--- options.sh
+++ options.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPENSSL_SOURCE="Source/openssl-1.1.1a"
+OPENSSL_SOURCE="Source/openssl-1.0.1g"
 HOST_PLATFORM="linux-x86_64"
 OPT_OPENSSL_SHARED=(1)
 ANDROID_NDK_ROOT="/root/NDK/android-ndk-r18b"
